60a78078fb4b61689e4e8f8c67deee5e8bdab61a
Tommy Gaessler <tommy.gaessler@zoom.us>
2024-09-04 11:15:44 -0600

    added image test

--- scripts.js
+++ scripts.js
@@ -27,7 +27,13 @@ var config = {
     sessionName: 'SessionA',
     userName: 'UserA',
     sessionPasscode: 'abc123',
-    features: ['preview', 'video', 'audio', 'settings', 'users', 'chat', 'share']
+    features: ['preview', 'video', 'audio', 'settings', 'users', 'chat', 'share'],
+    options: { init: {}, audio: {}, video: {}, share: {}},
+    virtualBackground: {
+       allowVirtualBackground: true,
+       allowVirtualBackgroundUpload: true,
+       virtualBackgrounds: ['https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop']
+    }
 };
 
 window.getVideoSDKJWT = getVideoSDKJWT
